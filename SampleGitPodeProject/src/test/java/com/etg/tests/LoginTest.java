411995a44c9b33e527360704b9b4e0d5446f8d60
lincy.verghese <lincy.verghese@indhydlpt-0211>
2023-01-10 12:27:22 +0530

    added test

---
+++ SampleGitPodeProject/src/test/java/com/etg/tests/LoginTest.java
@@ -0,0 +1,22 @@
+package com.etg.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LoginTest {
+  @Test
+  public void scenario1() throws InterruptedException {
+	  WebDriver driver;
+	  WebDriverManager.chromedriver().setup();
+      ChromeOptions options=new ChromeOptions();
+      options.setHeadless(true);
+      options.addArguments("window-size=1920,1200");
+      driver=new ChromeDriver(options);
+      driver.get("https://opensource-demo.orangehrmlive.com/");
+      Thread.sleep(2000);
+  }
+}
